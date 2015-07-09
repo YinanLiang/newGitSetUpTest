4934897fb01938c919ab746c1a0a2226f2a60e1c
Yinan Liang <yinanliang.au@gmail.com>
2015-07-09 11:05:29 +0800

    Add Codeigniter Frame Work

---
+++ application/models/mcontacts.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lynn
+ * Date: 6/29/15
+ * Time: 5:14 PM
+ */
+
+class MContacts extends Model {
+    function MContacts(){
+        parent::Model();
+    }
+
+    function addContact() {
+        $now = date("Y-m-d H:i:s");
+        $data = array (
+            "name" => $this->input->xss_clean($this->input->post("name")),
+            "email" => $this->input->xss_clean($this->input-post("email")),
+            "notes" => $this->input->xss_clean($this->input->post("notes")),
+            "ipaddress" => $this->input->ip_address(),
+            "stamp" => $now
+        );
+
+        $this->db->insert("contacts", $data);
+    }
+}
